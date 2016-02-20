707270197659763da06f5dcc6b15fc5a66062671
Oliver Vinn <oliver@vinn.co.uk>
2016-02-20 22:11:00 +0000

    Clean up

---
+++ src/clrun.py
@@ -0,0 +1,29 @@
+
+import clrunner
+
+__name__ == "__main__":
+
+    J_URL    = 'https://jenkins/jenkins/job/'
+    J_JOB    = 'Test'
+    C_URL    = 'http://coverity:8080'
+    C_PROJ   = 'TEST'
+    C_STREAM = 'NIGHTLY'
+	C_MAP    = 'MAP'
+	C_USER   = 'USER'
+	C_PASS   = 'PASSWORD'
+	M_SEARCH_PATH = 'build\\output'
+	M_FILE_EXT = '.qac.log'
+	
+	# Run from correct relative path
+	pass
+	# Build model
+    model = clrunner.get_model_desc(J_URL, J_JOB, 
+	                                C_URL, C_PROJ, C_STREAM, C_MAP, C_USER, C_PASS,
+									M_SEARCH_PATH, M_FILE_EXT,
+									PATH_STRIP,
+									include_jenkins=True,
+				                    include_coveritry=True)
+	# Convert
+	json_data = clrunner.model_to_jsonable(model)
+	# Export
+    print(json.dumps(json_data))
--- src/clrunner.py
+++ src/clrunner.py
@@ -14,10 +14,16 @@ from collections import defaultdict
 from cljenkins import Jenkins
 from clcoverity import Coverity
 import clutil
-from sys import stdout
 
 class SourceModel:
+    '''
+	Container for a set of source files
+	'''
     def __init__(self, strip_paths=None):
+	    '''
+		Args:
+			strip_paths (list(str))	- List of strings to remove from start of file path
+		'''
         self.items = {}
         self.item_lookup = defaultdict(list)
         self.groups = []
@@ -26,6 +32,8 @@ class SourceModel:
 
     def add_group(self, name, matches):
         '''
+		Defines a new source grouping rule
+		
         Args:
             name (str) - display name
             matches (list) - regex strings to match against
@@ -33,13 +41,31 @@ class SourceModel:
         self.group.append({'rule': name, 'component' : matches})
 
     def add(self, item):
+	    '''
+		Adds new item to the collection
+		
+		Args:
+			item (SourceItem) - Source item (file) to add to the collection
+		'''
+		# Strip string from path
         for i in self.strip_paths:
             if i and item.path.startswith(i):
                 item.path = item.path[len(i):]
+		# General collection
         self.items[item.path] = item
+		# Add for faster lookup in large collections
         self.item_lookup[item.path.split('/')[-1]].append(item)
 
     def getitem(self, path):
+		'''
+		Gets the requested source item
+		
+		Args:
+			path - full filename of a file to retrieve
+			
+		Returns:
+			SourceItem if found otherwise None
+		'''
         name = path.split('/')[-1]
         if name in self.item_lookup:
             subs = self.item_lookup[name]
@@ -49,14 +75,27 @@ class SourceModel:
         return None
 
 class SourceItem:
+	'''
+	Describes a source files quality indicators
+	'''
     def __init__(self, path=None, name=None, component=None, module=None):
+		'''
+		Args:
+			path (str) - full filename
+			name (str) - short name
+			component (str) - optional direct grouping attribute
+			module (str) - optional direct secondary grouping attribute
+		'''
+		# File attributes
         self.path = path
         self.name = name
         self.component = component
         self.module = module
+		# Quality attributes
         self.compiler_issues = []
         self.coverity_issues = []
         self.misra_issues = defaultdict(list)
+		# Computes factors
         if os.path.exists(path):
             self.size = clutil.file_source_size(path)
             self.conditions = clutil.file_condition_cnt(path)
@@ -68,47 +107,52 @@ class SourceItem:
             self.dependson = []
             
     def __repr__(self):
-        return "issues %d %d %d" %(len(self.compiler_issues), len(self.coverity_issues), len(self.misra_issues))
+        return "issues %d %d %d" %(len(self.compiler_issues), 
+		                           len(self.coverity_issues),
+								   len(self.misra_issues))
 
 
-def mixin_dependency(args):
+def mixin_dependencies(source_model, matches):
     '''
+	For a given list of files that represent unique source file outcomes
+	generates a unique fingerprint for comparison to determine true number
+	of compile outcomes.
+	
+    Args:
+        source_model (SourceModel) - source model description
+        matches (list) - 
     '''
-    d, matches = args
-    for l_file in matches:
-        name = os.path.basename(d['name'])
-        l_name = os.path.basename(l_file)
-        l_name = ".".join(l_name.split(".")[:-2])  # .o.lst
-        # sys.stderr.write(" ?    " + l_name + " <==> " + name + " " + d['path'] + "\n")
-        if 'fingerprints' in d:
-            if l_name == name and d['component'] in l_file and d['module'] in l_file:
-                p = clutil.fingerprint_file(l_file)
-                if p in d['fingerprints']:
-                    d['fingerprints'][p].append(l_file)
-                else:
-                    d['fingerprints'][p] = [ l_file ]
-                matches.remove(l_file)
-                sys.stderr.write("Finished " + l_file + "\n")
+	for d in source_model:
+		for l_file in matches:
+			name = os.path.basename(d['name'])
+			l_name = os.path.basename(l_file)
+			l_name = ".".join(l_name.split(".")[:-2])  # .o.lst
+			if 'fingerprints' in d:
+				if l_name == name and d['component'] in l_file and d['module'] in l_file:
+					p = clutil.fingerprint_file(l_file)
+					if p in d['fingerprints']:
+						d['fingerprints'][p].append(l_file)
+					else:
+						d['fingerprints'][p] = [ l_file ]
+					matches.remove(l_file)
+					sys.stderr.write("Finished " + l_file + "\n")
 
-                for m in d['fingerprints'].values():
-                    lines = open(m[0]).readlines()
-                    for l in lines:
-                        if l.startswith("#line 1 "):
-                            d['depends-on'].append(l.strip()[9:-1])
-                d['depends-on'] = list(set(d['depends-on']))
+					for m in d['fingerprints'].values():
+						lines = open(m[0]).readlines()
+						for l in lines:
+							if l.startswith("#line 1 "):
+								d['depends-on'].append(l.strip()[9:-1])
+					d['depends-on'] = list(set(d['depends-on']))
+	return source_model
 
-def mixin_dependencies(description, matches):
-    for d in description:
-        mixin_dependency((d, matches))
-    return description
-
+	
 def mixin_compiler_warnings(source_model, warnings):
     '''
     Merge filesystem description and compiler warning data
 
     Args:
-        description (list):
-        warnings (list):
+        source_model (SourceModel) - source model description
+        warnings (list) - warnings to merge into model
     '''
     for w in warnings.keys():
         m = None
@@ -122,13 +166,14 @@ def mixin_compiler_warnings(source_model, warnings):
         m.compiler_issues.extend(warnings[w])
     return source_model
 
+
 def mixin_coverity_warnings(source_model, warnings):
     '''
     Merge filesystem description and coverity warning data
 
     Args:
-        description (list):
-        warnings (list):
+        source_model (SourceModel) - source model description
+        warnings (list) - warnings to merge into model
     '''
     for w in warnings:
         m = None
@@ -142,7 +187,13 @@ def mixin_coverity_warnings(source_model, warnings):
         m.coverity_issues.append(w.__dict__)
     return source_model
 
+
 def mixin_qac_warnings(source_model, files):
+	'''
+    Args:
+        source_model (SourceModel) - source model description
+        files (list) - warnings files to scan for issues and merge into model
+	'''
     wr = re.compile('(.*)\\((.*)\\) \\+\\+ WARNING \\+\\+: <=([7-8])=(.*)')
     sys.stderr.write("Found %s files \n" % len(files))
     for f in files:
@@ -157,100 +208,93 @@ def mixin_qac_warnings(source_model, files):
                     source_model.add(m)
                 m.misra_issues[int(warning_match.group(3))].append({'line' : warning_match.group(2),
                                                                     'detail' : warning_match.group(4)})
-
     return source_model
 
-def get_description(jenkins_url, jenkins_job, coverity_url, coverity_project, coverity_stream):
+def get_model_desc(jenkins_url, jenkins_job, 
+                   coverity_url, coverity_project, coverity_stream, coverity_map, user, password,
+				   misra_file_search_path, misra_file_extension,
+				   strip_paths,
+				   include_jenkins=True,
+				   include_coveritry=True):
     '''
-    Gets a rich description of the cm location agreegating file sytem and analytic sources
+    Gets a rich description of the cm location agreegating file sytem and analytic sources.
+	
+	Args:
+		jenkins_url (str)      - base url for instance
+		jenkins_job (str)      - name of job
+		coverity_url (str)     - base url for instance
+		coverity_project (str) - name of coverity project
+		coverity_stream (str)  - name of coverity stream in project 
+		coverity_map (str)     - name of coverity component map
+		user (str)             - user to acccess coverity 
+		password (str)         - users password
+	
+	Returns:
+		Source Model
+
+	Limitations:
+		Minimal (no) error protection for server being down.
     '''
     from datetime import datetime
 
-	enb_jenkins = True
-    enb_coverity = True
-        
-    # Ensure relative path is ok
-    os.chdir('..')
-
     # Build local source tree (list paths to be stripped)
-    cm = SourceModel(['<mypath>',
-                      '<another>'])
+    model = SourceModel(strip_paths)
     
-	sys.stderr.write("Scanning local QAC data \n")
+	sys.stderr.write("Scanning local QAC data\n")
     a = datetime.now()
-    cm = mixin_qac_warnings(cm, clutil.find_files('build\\output\\', '.qac'))
+    model = mixin_qac_warnings(model, clutil.find_files(misra_file_search_path, misra_file_extension))
 
-    if enb_jenkins:
+    if include_jenkins:
         b = datetime.now() - a
-        sys.stderr.write(str(b) +  "\nFetching Jenkins Data from Server\n")
+        sys.stderr.write(str(b) + "Fetching Jenkins Data from Server\n")
         a = datetime.now()
         j = Jenkins(jenkins_url)
         jd = j.fetch_latest_warnings(jenkins_job)
-        sys.stderr.write("\n".join([str(x) for x in jd]))
         sys.stderr.write("Total: %d\n" %(len(jd)))
         
-    if enb_coverity:
+    if include_coveritry:
         b = datetime.now() - a
-        sys.stderr.write(str(b) + "\nFetching Coverity Data from Server \n")
+        sys.stderr.write(str(b) + "Fetching Coverity Data from Server\n")
         a = datetime.now()
-        c = Coverity(coverity_url, 'USER', 'PASS')
-        cm.groups = c.fetch_component_map('MAPPING')
-        cm.group_stats = c.fetch_project_stats(coverity_project, cm.groups)
+        c = Coverity(coverity_url, user, password)
+        model.groups = c.fetch_component_map(coverity_map)
+		for k in groups:
+			k['rule'] = re.compile(k['rule'])
+        model.group_stats = c.fetch_project_stats(coverity_project, model.groups)
         cd = c.fetch_outstanding_stream_defects(coverity_project, coverity_stream)
-        sys.stderr.write("\n".join([str(x) for x in cd]))
         sys.stderr.write("Total: %d\n" %(len(cd)))
         
-    if enb_jenkins:
+    if include_jenkins:
         b = datetime.now() - a
-        sys.stderr.write(str(b) +  "\nMerging Compiler Data\n")
+        sys.stderr.write(str(b) + "Merging Compiler Data\n")
         a = datetime.now()
-        cm = mixin_compiler_warnings(cm, jd)
+        model = mixin_compiler_warnings(model, jd)
     
-    if enb_coverity:
+    if include_coveritry:
         b = datetime.now() - a
-        sys.stderr.write(str(b) +  "\nMerging Coverity Data\n")
+        sys.stderr.write(str(b) +  "Merging Coverity Data\n")
         a = datetime.now()
-        cm = mixin_coverity_warnings(cm, cd)
+        model = mixin_coverity_warnings(model, cd)
         b = datetime.now() - a
         sys.stderr.write(str(b))
-        
-    return cm
-
-if __name__ == "__main__":
-
-    J_URL    = 'https://jenkins/jenkins/job/'
-    J_JOB    = 'Test'
-    C_URL    = 'http://coverity:8080'
-    C_PROJ   = 'Ford_GEN4'
-    C_STREAM = 'B515_NIGHTLY'
-
-    model = get_description(J_URL, J_JOB, C_URL, C_PROJ, C_STREAM)
-
-    groups = model.groups
-    groups.append({'component' : 'OTHER', 'rule' : re.compile('.*')})
-
-    # group
-    #   name: component
-    #   files:    files
     
-    for k in groups:
-        k['rule'] = re.compile(k['rule'])
-    
-    items = defaultdict(list)
+    model.groups.append({'component' : 'OTHER', 'rule' : re.compile('.*')})
+    return model
+	
+def model_to_jsonable(model):
+	items = defaultdict(list)
     for d,v in model.items.items():
         for k in groups:
             if k['rule'].match(d):
                 items[k['component']].append(v)
                 break;
-
     # Add empty component groups
     for i in groups:
         if i['component'] not in items.keys():
             items[i['component']] = []
-
     # Sorted order
     names = sorted(items.keys())
     out = [{'name': x, 'files' : [y.__dict__ for y in items[x]]} for x in names]
+	return {'data': out, 'stats' : model.group_stats }
 
-    print(json.dumps({'data': out, 'stats' : model.group_stats }))
  
